fcbbaa439e1601b3b720f3866b5b22bb36f800d8
paiiiigggeeee <91573998+paiiiigggeeee@users.noreply.github.com>
2024-02-07 12:32:42 -0600

    Add files via upload

---
+++ firstGUI.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+
+def main():
+    firstGUI().mainloop()
+
+
+class firstGUI(Frame):
+
+    def __init__(self):
+        Frame.__init__(self)
+        self.master.title("My First GUI")
+        self.grid()
+        self._label = Label(self, text = "I'm a label!")
+        self._label.grid()
+
+    main()
+
+
